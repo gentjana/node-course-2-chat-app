a43125ce77c7783d58a9d55b6c85ec1253c2e468
Gentjana Malasi <gentjana.malasi@gmail.com>
2018-07-19 15:03:35 +0200

    emit newMessage on createMessage

--- server/server.js
+++ server/server.js
@@ -15,24 +15,13 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
     console.log('New user connected');
 
-    socket.emit('newEmail', {
-        from: 'mike@example.com',
-        text: 'Hey. What is going on.',
-        createdAt: 123
-    });
-
-    socket.emit('newMessage', {
-        from: 'mike',
-        text: 'Hello how are you?',
-        createdAt: 1234
-    });
-
-    socket.on('createEmail', (email) => {
-        console.log('createEmail', email);
-    });
-
     socket.on('createMessage', (message) => {
         console.log('createMessage', message);
+        io.emit('newMessage', {
+            from: message.from,
+            text: message.text,
+            createdAt: new Date().getTime()
+        });
     });
 
     socket.on('disconnect', () => {
